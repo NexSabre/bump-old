b80155b932c3362cc35660cf7d1bc5768b8e6ccb
Nex Sabre <NexSabre@users.noreply.github.com>
2020-08-02 15:31:42 +0200

    WIP

--- version.py
+++ version.py
@@ -3,7 +3,7 @@ class Version:
         self.major: int = 0
         self.minor: int = 0
         self.patch: int = 0
-        self.additinal: string = None
+        self.additinal: str = None
         self.build: int = 0
 
         self._list_version = self.list_version()
@@ -13,19 +13,30 @@ class Version:
         return [self.major, self.minor, self.patch, self.additinal, self.build]
 
     def read_version_file(self):
-        with open('VERSION', 'r') as version_file: 
-            ver_information = version_file.read().split('.')
-            self._len_version = len(ver_information)
+        try:
+            with open('VERSION', 'r') as version_file: 
+                ver_information = version_file.read().split('.')
+                self._len_version = len(ver_information)
 
-            for i, v in enumerate(ver_information):
-                self._list_version[i] = v
+                for i, v in enumerate(ver_information):
+                    self._list_version[i] = v
+        except FileNotFoundError as error:
+            print(error)
 
     def __str__(self):
         types = []
-        for i in self._len_version:
-            types.append(self.list_version[i])
+        for i in range(self._len_version):
+            types.append(self._list_version[i])
         return '.'.join(types)
 
+    def save_to_file(self, filename: str = 'VERSION'):
+        with open(filename, 'w') as version_file:
+            version_file.write(self.__str__())
 
 if __name__ == "__main__":
-    v = Version().read_version_file()
+    v = Version()
+    v.
+    
+    ()
+    v.save_to_file()
+    print(v)
