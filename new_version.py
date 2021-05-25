624db57ea2f91cf4a53f4ddcb924200f7b19b07c
Nex Sabre <NexSabre@users.noreply.github.com>
2021-05-25 21:10:01 +0200

    .

--- new_version.py
+++ new_version.py
@@ -1,23 +1,45 @@
-from typing import Any, AnyStr
+from sys import version
+from typing import Any, AnyStr, List
 
 import os
 
 
 class Version:
-    def __init__(self) -> None:
-        self.major: int = 0
-        self.minor: int = 0
-        self.path: int = 0
+    def __init__(self, raw: AnyStr) -> None:
+        self.raw: AnyStr = raw
+        self.raw_list: List[Any] = []
+        self.major: int = None
+        self.minor: int = None
+        self.patch: int = None
 
-    def minor_bump():
-        pass
+        self.__resolve_from_raw()
 
-    def patch_bump():
-        pass
+    def __str__(self) -> str:
+        return f"Version {self.major}.{self.minor}.{self.patch}"
+
+    def __resolve_from_raw(self):
+        self.raw_list = self.raw.split(".")
+        len_of_version = len(self.raw_list)
+
+        if len_of_version == 1:
+            self.major = int(self.raw_list[0])
+        elif len_of_version == 2:
+            self.major = int(self.raw_list[0])
+            self.minor = int(self.raw_list[1])
+        elif len_of_version == 3:
+            self.major = int(self.raw_list[0])
+            self.minor = int(self.raw_list[1])
+            self.patch = int(self.raw_list[2])
+
+    def minor_bump(self):
+        self.minor += 1
+
+    def patch_bump(self):
+        self.patch += 1
 
 
 class Bump:
-    def __init__(self, filepath=None) -> None:
+    def __init__(self, filepath: AnyStr = None) -> None:
         self.filepath: AnyStr = None
         self.version: AnyStr = None
 
@@ -33,13 +55,25 @@ class Bump:
                 "VERSION")
 
     def __open_version_file(self):
-        with open(self.filepath, "w") as f:
-            file_version = f.read()
+        with open(self.filepath) as f:
+            self.version = f.read()
 
+
+def reset_version(version: AnyStr = "0.1"):
+    with open("VERSION", "w") as version_file:
+        version_file.write(version)
 
 
 if __name__ == "__main__":
+    reset_version()
     bump = Bump()
     bump()
+    version = Version(raw=bump.version)
+    assert version.raw == "0.1"
 
-    
+    assert version.major == 0
+    assert version.minor == 1
+
+    # bump minor
+    version.minor_bump()
+    assert version.minor == 2
